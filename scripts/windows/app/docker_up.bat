ebf2c8966cb61c7380ef178f5ebe53d9221701df
Vladislav Filatov <vladislav.filatov@mego.travel>
2025-10-10 11:46:19 +0400

    chore: add docker local support

--- scripts/windows/app/docker_up.bat
+++ scripts/windows/app/docker_up.bat
@@ -1,5 +1,5 @@
 cd ..\..\..\
 
-docker compose -f deploy/docker/docker-compose.yml -f deploy/docker/docker-compose.local.yml up -d --build
+docker compose -f deploy/docker/docker-compose.yml up -d --build
 
 pause
--- scripts/windows/app/docker_up_new.bat
+++
@@ -1,5 +0,0 @@
-cd ..\..\..\
-
-env $(cat ../env/local.env | xargs) docker compose -f deploy/docker/docker-compose.yml -f deploy/docker/docker-compose.local.yml up -d --build
-
-pause
