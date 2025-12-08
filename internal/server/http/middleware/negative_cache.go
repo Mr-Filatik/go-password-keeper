102df1af9b28d128160002414332989b8fba3c90
Vladislav Filatov <vladislav.filatov@mego.travel>
2025-12-08 16:39:07 +0400

    feat: add file for negative cache

---
+++ internal/server/http/middleware/negative_cache.go
@@ -0,0 +1,7 @@
+// Package middleware provides functionality for HTTP middleware.
+package middleware
+
+//nolint:godox
+// TODO: Implement.
+// Negative404CacheMiddleware
+// NotFoundCacheMiddleware
