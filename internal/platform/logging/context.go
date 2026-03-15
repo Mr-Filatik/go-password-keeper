b874c7995ce9e31373b6e67e13a751b3eff48dc7
Vladislav Filatov <vladislav.filatov@mego.travel>
2026-03-16 00:34:16 +0400

    feat: update logger middleware

--- internal/platform/logging/context.go
+++ internal/platform/logging/context.go
@@ -7,23 +7,23 @@ import (
 	contextpkg "github.com/mr-filatik/go-password-keeper/internal/platform/context"
 )
 
-// CtxKeyLogger structure for storing and searching for a logger in a context.
+// ctxLoggerKey structure for storing and searching for a logger in a context.
 //
-//nolint:gochecknoglobals // Migrate from platform to http
-var CtxKeyLogger = &contextpkg.CtxKey{Name: "logger"}
+//nolint:gochecknoglobals
+var ctxLoggerKey = &contextpkg.CtxKey{Name: "logger"}
 
 // ToContext sets the logger to the context.
 func ToContext(ctx context.Context, logger Logger) context.Context {
-	return context.WithValue(ctx, CtxKeyLogger, logger)
+	return context.WithValue(ctx, ctxLoggerKey, logger)
 }
 
 // FromContext gets the logger from the context.
 //
 //nolint:ireturn // Necessary to fix the function in the interface.
 func FromContext(ctx context.Context) Logger {
-	value := ctx.Value(CtxKeyLogger)
+	value := ctx.Value(ctxLoggerKey)
 	if value == nil {
-		return nil
+		return nil // return &noopLogger{}
 	}
 
 	logger, ok := value.(Logger)
--- internal/server/http/middleware/inject_logger.go
+++ internal/server/http/middleware/inject_logger.go
@@ -8,9 +8,6 @@ import (
 )
 
 // InjectLogger represents middleware for inject logger into context.
-//
-// Parameters:
-//   - logger logging.Logger: logger.
 func InjectLogger(logger logging.Logger) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
